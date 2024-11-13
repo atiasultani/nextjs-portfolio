931bad0e13eac02e20a9ff24981473637b077a8c
atiasultani <160133182+atiasultani@users.noreply.github.com>
2024-11-14 02:41:30 +0500

    portfolio

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import img from "../../public/Image/img2.png";
 import linkedin from "../../public/Image/linkedin.png";
 import github from "../../public/Image/github.jpg";
 import map from "../../public/Image/map.jpg";
@@ -12,202 +11,128 @@ import atmpro from "../../public/Image/atm.jpg";
 import cart from "../../public/Image/c-art.jpg";
 import cwrite from "../../public/Image/con-write.jpg";
 
-
-
 const page = () => {
   return (
     <div className="maindiv">
-      <div className="header">
-        <Image
-          src={img}
-          alt="img"
-          width="58"
-          height="48"
-          className="styleimage"
-        />
-        <h1 className="h1">A<span>tia</span> S<span>ultani</span></h1>
-        <Link className="bton" href="#home">
-          Home
-        </Link>
-        <Link className="bton" href="#about">
-          About
-        </Link>
-        <Link className="bton" href="#services">
-          Services
-        </Link>
-        <Link className="bton" href="#contact">
-          Contact
-        </Link>
-      </div>
-
-      <div className="intro">
-        <div className="log">
+      <div className="header flex items-center justify-between px-4 py-6 bg-gray-800">
+        <div className="flex items-center">
           <Image
             src={logo}
             alt="img"
-            width="250"
-            height="230"
-            className="logo"
+            width="58"
+            height="48"
+            className="styleimage"
           />
+          <h1 className="text-white text-3xl ml-2">A<span className="text-yellow-400">tia</span> S<span className="text-yellow-400">ultani</span></h1>
         </div>
-        <div className="info">
-          <h3>
-
-            I am
-            <p>
-              A<span>tia</span> S<span>ultani</span>
-            </p>
-          </h3>
-          <h1> Front End Developer </h1>
-          <p>
-            <q> Your Code Is The Foundation Of Someone`s Next Wow  Moment </q>
-          </p>
+        <div className="hidden md:flex space-x-6">
+          <Link className="bton text-white" href="#home">Home</Link>
+          <Link className="bton text-white" href="#about">About</Link>
+          <Link className="bton text-white" href="#services">Services</Link>
+          <Link className="bton text-white" href="#contact">Contact</Link>
         </div>
-
-
       </div>
 
-      <section id="about">
-        <h1>Hello ! </h1>
-        <h3> Here You Read My Introducation  </h3>
-        <p>
-          Recently I Continued My Web Developement Course in GIAIC<br />
-          (Governor Sindh Initiative for
-          Artificial Intelligence, Web 3.0 & Metaverse).
-          <br />
-          I done HTML , CSS , JAVASCRIPT , TYPESCRIPT , NEXT JS
+      <div className="intro flex flex-col md:flex-row items-center px-4 py-16 bg-gray-900">
+        <div className="log mb-8 md:mb-0">
+          <Image src={logo} alt="img" width="250" height="230" className="logo rounded-full" />
+        </div>
+        <div className="info text-center md:text-left text-white">
+          <h3 className="text-xl font-semibold">I am</h3>
+          <p className="text-2xl font-bold">A<span className="text-yellow-400">tia</span> S<span className="text-yellow-400">ultani</span></p>
+          <h1 className="text-4xl font-extrabold">Front End Developer</h1>
+          <p className="italic text-lg mt-4">"Your Code Is The Foundation Of Someone’s Next Wow Moment"</p>
+        </div>
+      </div>
+
+      <section id="about" className="px-4 py-16 bg-gray-800 text-white">
+        <h1 className="text-3xl font-bold text-center">Hello !</h1>
+        <h3 className="text-xl text-center mt-4">Here You Read My Introduction</h3>
+        <p className="mt-4 text-center text-lg">
+          Recently, I continued my Web Development course at GIAIC (Governor Sindh Initiative for Artificial Intelligence, Web 3.0 & Metaverse). I have worked with HTML, CSS, JavaScript, TypeScript, and Next.js.
         </p>
-        <p className="text">You May Further Vist My Profile on Linkin.Click on it</p>
-        <Link href="https://www.linkedin.com/in/atia-sultani-a9520113b" target="_blank">
-
-          <Image
-            src={linkedin}
-            alt="linkedin"
-            width={70}
-            height={70}
-            className="linkedin"
-          />
-        </Link><br />
-        <p className="text">At Github You Can see My Work & projects.Click on it</p>
-        <Link href="https://github.com/atiasultani" target="_blank">
-          <Image
-            src={github}
-            alt="github"
-            width={70}
-            height={70}
-            className="github"
-          />
-        </Link>
-
-      </section>
-
-      <section id="services">
-        <div className="serv-main">
-
-          <div className="ser">
-
-            <h1>Web Developement</h1>
-            <Link href="https://firstweb-rosy.vercel.app/" target="_blank">
-
-              <Image
-                src={webimg}
-                alt="web designs"
-                width={150}
-                height={150}
-                className="s-div"
-              />
-            </Link><br />
-          </div>
-
-          <div className="ser">
-            <h1> Typescript Project</h1>
-            <Link href="https://github.com/atiasultani/as4-ATM-Transaction" target="_blank">
-
-              <Image
-                src={atmpro}
-                alt="atm project"
-                width={150}
-                height={150}
-                className="s-div"
-              />
-            </Link><br />
-
-          </div>
-
-          <div className="ser">
-            <h1>Logo Design</h1>
-
+        <div className="text-center mt-6">
+          <p className="text-lg">You May Further Visit My Profile on LinkedIn. Click on it.</p>
+          <Link href="https://www.linkedin.com/in/atia-sultani-a9520113b" target="_blank">
             <Image
-              src={logodes}
-              alt="logo designs"
-              width={150}
-              height={150}
-              className="s-div"
+              src={linkedin}
+              alt="linkedin"
+              width={70}
+              height={70}
+              className="inline-block my-4"
             />
-
-          </div>
-
-          <div className="ser">
-
-            <h1>Creative Art</h1>
+          </Link>
+          <p className="text-lg mt-4">At GitHub, you can see my work & projects. Click on it.</p>
+          <Link href="https://github.com/atiasultani" target="_blank">
             <Image
-              src={cart}
-              alt="creative art "
-              width={150}
-              height={150}
-              className="s-div"
+              src={github}
+              alt="github"
+              width={70}
+              height={70}
+              className="inline-block my-4"
             />
-
-          </div>
-
-          <div className="ser">
-
-            <h1>Contant Writer</h1>
-            <Image
-              src={cwrite}
-              alt="creative art "
-              width={150}
-              height={150}
-              className="s-div"
-            />
-
-          </div>
-
-
+          </Link>
         </div>
       </section>
 
-
-      <section id="contact">
-        <address>
-          <p>
-            <b> Mail To : </b> Atia.Sultani@outlook.com
-          </p>
-          <p>Visit Our Social Media Plateform</p>
-          <p>Click on it</p>
-          <div className="social">
-
-            <a className="a" href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank">
-              <Image className="pic" src="/facebook.jpg" alt="Facebook" title="Facebook" width={70} height={70} />
-            </a>
-
-            <a className="a" href="https://www.instagram.com/" target="_blank">
-              <Image className="pic" src="/instagram.jpg" alt="Instagram" title="Instagram" width={70} height={70} />
-            </a>
-
-            <a className="a" href="https://www.youtube.com/" target="_blank">
-              <Image className="pic" src="/youtube.jpg" alt="Youtube" title="Youtube" width={70} height={70} />
-            </a>
-
+      <section id="services" className="bg-gray-900 px-4 py-16 text-white">
+        <h1 className="text-3xl font-bold text-center">Services</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+          <div className="ser text-center">
+            <h1 className="text-xl font-semibold">Web Development</h1>
+            <Link href="https://firstweb-rosy.vercel.app/" target="_blank">
+              <Image src={webimg} alt="web designs" width={150} height={150} className="rounded-full mt-4" />
+            </Link>
           </div>
 
-          <p>
-            <b> Whats App : </b> 0321-123456789
+          <div className="ser text-center">
+            <h1 className="text-xl font-semibold">Typescript Project</h1>
+            <Link href="https://github.com/atiasultani/as4-ATM-Transaction" target="_blank">
+              <Image src={atmpro} alt="atm project" width={150} height={150} className="rounded-full mt-4" />
+            </Link>
+          </div>
+
+          <div className="ser text-center">
+            <h1 className="text-xl font-semibold">Logo Design</h1>
+            <Image src={logodes} alt="logo designs" width={150} height={150} className="rounded-full mt-4" />
+          </div>
+
+          <div className="ser text-center">
+            <h1 className="text-xl font-semibold">Creative Art</h1>
+            <Image src={cart} alt="creative art" width={150} height={150} className="rounded-full mt-4" />
+          </div>
+
+          <div className="ser text-center">
+            <h1 className="text-xl font-semibold">Content Writer</h1>
+            <Image src={cwrite} alt="creative art" width={150} height={150} className="rounded-full mt-4" />
+          </div>
+        </div>
+      </section>
+
+      <section id="contact" className="bg-gray-800 px-4 py-16 text-white">
+        <address className="text-center">
+          <p className="text-lg">
+            <b>Mail To:</b> Atia.Sultani@outlook.com
           </p>
-          <p>
-            <b> Address:</b> office 000 block 0 street 0 , Pakistan
+          <p className="text-lg mt-4">Visit Our Social Media Platform</p>
+          <div className="social flex justify-center mt-4">
+            <a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank">
+              <Image className="pic mx-4" src="/facebook.jpg" alt="Facebook" title="Facebook" width={70} height={70} />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank">
+              <Image className="pic mx-4" src="/instagram.jpg" alt="Instagram" title="Instagram" width={70} height={70} />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank">
+              <Image className="pic mx-4" src="/youtube.jpg" alt="Youtube" title="Youtube" width={70} height={70} />
+            </a>
+          </div>
+          <p className="mt-4">
+            <b>WhatsApp:</b> 0321-123456789
           </p>
-          <Image src={map} alt="map" className="map" />
+          <p className="mt-2">
+            <b>Address:</b> Office 000 block 0 street 0, Pakistan
+          </p>
+          <Image src={map} alt="map" className="map mt-8 mx-auto" />
         </address>
       </section>
     </div>
